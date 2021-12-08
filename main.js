a379cea82c9e426485c89f855a778ccc85fd288d
AlineDB <71900695+AlineDB@users.noreply.github.com>
2021-12-08 16:42:42 +0100

    init1

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 // Nom : 
 // Prénom : 
-// Groupe : 
+// Groupe :
+const App={
+    init(){
+        document.documentElement.classList.add('js-enabled');
+        (function () {
+            const eOl = document.querySelector('#app');
+            for (iCompteur=1;iCompteur<200;iCompteur+2){
+                if (iCompteur % 3 === 0){
+                    eOl.insertAdjacentElement('beforeend',`<li class="grid__item"></li><li class="multiple-3 grid__item animate">${iCompteur}<div class="ribbon-wrapper">
+    <div class="ribbon">3</div></div></li>`);
+                } else if (iCompteur % 3 === 0 && iCompteur % 9 === 0){
+                    eOl.insertAdjacentElement('beforeend',`<li class="multiple-3-9 grid__item">${iCompteur}<div class="ribbon-wrapper"><div class="ribbon">3 et 9</div></div></li>`);
+                } else if (iCompteur % iCompteur === 0){
+                    eOl.insertAdjacentElement('beforeend',`<li data-sum="6" class="premier grid__item">${iCompteur}<div class="ribbon-wrapper"><div data-text="somme" class="ribbon">premier</div></div></li>`);
+                }
+            }
+        })();
+    }
+}
+
+
+App.init();
+
+
+
